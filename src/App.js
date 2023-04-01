60a23398a3cf7f9c615dd3829cb6ec13963f1574
Sarthak Sharma <ssarthak116@protonmail.ch>
2023-04-02 01:00:01 +0530

    routing, emergency commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // import './App.css';
-// import Signup from './components/Signup/Signup.jsx'
+import Signup from './components/Signup/Signup.jsx'
 import TopBar from './components/TopBar.jsx';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <TopBar />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Signup />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/test">
+            <TopBar />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
-export default App;
