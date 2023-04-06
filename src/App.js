528b8c2a7804ec74c31e67c67ff60c5fc29e6b76
Sarthak Sharma <sarthak.s@gsl.in>
2023-04-06 23:45:49 +0530

    dependencies clear + top bar progress

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import Signup from './components/Signup/Signup.jsx'
 import TopBar from './components/TopBar.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/test" element={<TopBar />}/>
+        <Route path="/test" element={<TopBar />} />
+        <Route path="/home" element={<Home />} />
         {/* <Route path ="*" element= {<NoMatch />}/>  */}
       </Routes>
     </BrowserRouter>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import './../assets/styles/TopBar.css';
+import Button from '@mui/material/Button';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+
 
 export default function TopBar() {
 
     return (
         <>
             <div className="top-bar">
-                This is the top bar.
+                <div className="content">
+                    <Button variant="text"><HomeOutlinedIcon /> </Button>
+                </div>
             </div>
         </>
     );
